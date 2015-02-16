7b05bf75b71b4c56c923568b8f6bf9b6d7227a13
KakitC <kitkatcheung@gmail.com>
2015-02-16 12:41:10 -0800

    Initialize piCamRender
    Testing script for running Rpi camera and overlaying the coordinates
    calculated from colorCOMfinder.
    
    Initial test just to check that all the overlays work.

---
+++ piCamRender/picam_render00.py
@@ -0,0 +1,26 @@
+import picamera as pic
+from PIL import Image as img
+import numpy as np
+import time
+from ..colorCOMfinder import colorCOMfinder05
+
+res = (80, 60)
+red, red_rgb = (0, 127, 255), (255, 0, 0)
+thresh = (15, 30, 30)
+
+with pic.PiCamera() as cam:
+    cam.resolution = res
+    cam.start_preview()
+
+    o = np.zeros((res[0], res[1], 3), dtype=np.uint8)
+
+    olay = cam.add_overlay(np.getbuffer(o), layer=3, alpha=64)
+    try:
+        while True:
+            for i in range(res[0]-3):
+                o = np.zeros((res[0], res[1], 3), dtype=np.uint8)
+                o[i:i+3, :, :] = 255
+                olay.update(np.getbuffer(o))
+                time.sleep(.2)
+    finally:
+        cam.remove_overlay(olay)

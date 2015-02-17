04cb596c69266397ef3ef5a0ceb18381ea8f637f
VexUBC RaspPi <VexUBC@gmail.com>
2015-02-17 05:32:36 +0000

    Pi camera testing
    COM finder not working on pi camera during taking images,
    doesn't return coordinates. It's probably the quality of the image,
    which causes weird colors. Check the colors in the picture to make sure
    everything is working fine.
    
    Added sample pictures from Rpi

--- piCamRender/picam_render00.py
+++ piCamRender/picam_render00.py
@@ -1,7 +1,10 @@
+# Feb 16 2015
+
 import picamera as pic
 from PIL import Image as img
 import numpy as np
 import io
+import time
 from colorCOMfinder import colorCOMfinder05 as comf5
 
 
@@ -29,28 +32,33 @@ def make_overlay(coords, res):
 
 def render_target():
     res = (80, 60)
-    target_red = (0, 127, 255)
-    thresh = (15, 30, 30)
+    target_red = (0, 100, 255)
+    thresh = (20, 20, 40)
 
     with pic.PiCamera() as cam:
         cam.resolution = res
 
         opad = make_overlay((-1, -1), res)
-        olay = cam.add_overlay(opad.tostring(), size=(res[1], res[0], 3),
+        olay = cam.add_overlay(opad.tostring(), size=(res[0], res[1]),
                                layer=3, alpha=96)
         stream = io.BytesIO()
         target_range = comf5.gen_rgb_range(target_red, thresh)
+        print(target_range)
         px_search_list = comf5.gen_px_list(res)
 
-        cam.start_preview()
-
         try:
-            while True:
+            cam.start_preview()
+            t0 = time.time()
+            while time.time() - t0 < 8:
                 cam.capture(stream, format='jpeg')
                 stream.seek(0)
                 image = img.open(stream)
                 coords = comf5.color_com(image, target_range, px_search_list)
+                print(coords)
                 opad = make_overlay(coords, res)
-                olay.update(opad)
+                olay.update(opad.tostring())
+                time.sleep(.05)
+                print(time.time() - t0)
         finally:
             cam.remove_overlay(olay)
+            cam.stop_preview()

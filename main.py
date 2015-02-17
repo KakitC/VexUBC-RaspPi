01a16f06f1e4bf7118da6ba222a291877a6791de
KakitC <kitkatcheung@gmail.com>
2015-02-16 20:02:13 -0800

    Added targeting to picam_render
    Added targeting functionality from colorCOMfinder to render on Rpi
    camera preview, hopefully it works

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from piCamRender import picam_render00 as pcr0
 from colorCOMfinder import colorCOMfinder05 as comf5
 
-pcr0.run()
+pcr0.render_target()
 
--- piCamRender/picam_render00.py
+++ piCamRender/picam_render00.py
@@ -1,40 +1,56 @@
 import picamera as pic
 from PIL import Image as img
 import numpy as np
-import time
+import io
 from colorCOMfinder import colorCOMfinder05 as comf5
 
 
-def run():
+def make_overlay(coords, res):
+    """ Creates a padded image for overlaying the camera preview, with
+        a dot marking the position of the target coordinates
+
+        Params:
+            coords - <2-tuple> Coordinates of target position in image
+            res - <2-tuple> Camera resolution
+        Returns:
+            <Image> Image containing targeting dot, padded to overlay buffer size
+    """
+
+    o = np.zeros((res[1], res[0], 3), dtype=np.uint8)
+    if 0 <= coords[0] < res[0] and 0 <= coords[1] < res[1]:
+        o[coords[1], coords[0], :] = 0xff
+    omg = img.fromarray(o)
+    opad = img.new('RGB', (
+             ((res[0] + 31) // 32) * 32,
+             ((res[1] + 15) // 16) * 16))
+    opad.paste(omg, (0, 0))
+    return opad
+
+
+def render_target():
     res = (80, 60)
-    red, red_rgb = (0, 127, 255), (255, 0, 0)
+    target_red = (0, 127, 255)
     thresh = (15, 30, 30)
 
     with pic.PiCamera() as cam:
         cam.resolution = res
-        cam.start_preview()
 
-        o = np.zeros((res[1], res[0], 3), dtype=np.uint8)
-        o[40, :, :] = 0xff
-        omg = img.fromarray(o)
-        opad = img.new('RGB', (
-             ((res[0] + 31) // 32) * 32,
-             ((res[1] + 15) // 16) * 16))
-        opad.paste(omg, (0,0))
-        olay = cam.add_overlay(opad.tostring(), size=omg.size,
-                               layer=3, alpha=64)
+        opad = make_overlay((-1, -1), res)
+        olay = cam.add_overlay(opad.tostring(), size=(res[1], res[0], 3),
+                               layer=3, alpha=96)
+        stream = io.BytesIO()
+        target_range = comf5.gen_rgb_range(target_red, thresh)
+        px_search_list = comf5.gen_px_list(res)
+
+        cam.start_preview()
 
         try:
             while True:
-                for i in range(res[1])[::-1]:
-                    o = np.zeros((res[1], res[0], 3), dtype=np.uint8)
-                    o[i, :, :] = 0xff
-                    omg = img.fromarray(o)
-                    opad = img.new('RGB', (
-                         ((res[0] + 31) // 32) * 32,
-                         ((res[1] + 15) // 16) * 16))
-                    opad.paste(omg, (0,0))
-                    olay.update(opad.tostring())
-                    time.sleep(.2)
+                cam.capture(stream, format='jpeg')
+                stream.seek(0)
+                image = img.open(stream)
+                coords = comf5.color_com(image, target_range, px_search_list)
+                opad = make_overlay(coords, res)
+                olay.update(opad)
         finally:
             cam.remove_overlay(olay)
